1b85904182d23e28d250d5d22d69e86a8a118922
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-05 16:45:53 +0000

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat two strings and return the concatenated string"""
+    return str1 + str2
