e537c1cca506eefe567c0506c3393e79fd3bee31
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-10 09:11:29 +0000

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a coroutine called async_generator that takes no arguments.
+"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """loop 10 times, each time asynchronously wait 1 second"""
+    n: int = 10
+    delay: int = 1
+    for i in range(n):
+        await asyncio.sleep(delay)
+        random_number = random.uniform(0, n)
+        yield random_number
